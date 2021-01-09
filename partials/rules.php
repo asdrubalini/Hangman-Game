f416642a4a5d2e5c2b35238e52bfb6517c1cf899
Giovanni <indirizzoemail@mail.com>
2021-01-09 19:43:16 +0100

    edit rules

--- partials/rules.php
+++ partials/rules.php
@@ -1,11 +1,19 @@
 <h2>Regole del gioco</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et metus et eros vestibulum dictum sed vel purus. Morbi ante enim, placerat vitae finibus a, mollis eu mauris. Aliquam erat volutpat. Nulla vel diam quis felis lacinia vulputate id et libero. Cras maximus, tortor tempor aliquam auctor, massa nibh facilisis lectus, quis cursus nulla tortor in turpis. Aenean sed iaculis ipsum. Aliquam ut mi felis. Proin sed efficitur orci. Donec vestibulum purus in sodales aliquam. Quisque aliquet volutpat accumsan. Nam euismod urna eu massa laoreet blandit.</p>
-<p>Sed non dui id lacus eleifend posuere ut ac justo. Donec et nulla eget orci pulvinar tempor. Proin id ante dictum, interdum eros id, gravida ligula. Etiam pulvinar massa in elit tempor, id bibendum ligula scelerisque. Donec eget sollicitudin est. Vivamus scelerisque posuere massa, at malesuada erat feugiat sed. Mauris velit tortor, gravida at gravida vestibulum, lobortis cursus nisl. Pellentesque bibendum quis lectus a mattis. Nam ultrices orci ut nisi aliquam, non varius massa efficitur. Vestibulum ac nulla posuere, maximus nibh vitae, feugiat diam. Sed a pellentesque metus, vitae vestibulum justo. Phasellus mauris augue, scelerisque in sollicitudin sed, aliquet sed risus. Duis pretium turpis ut tortor iaculis, sit amet malesuada neque rhoncus. Quisque non risus vel mauris pulvinar pretium vel sed velit. Curabitur vel odio in quam lobortis porttitor.</p>
+
+<p>L'impiccato è un famosissimo gioco che consiste nel provare ad indovinare una frase nascosta,
+lettera per lettera. Inizialmente sono vibili solamente la lunghezza di ogni parola all'interno
+della frase e la posizione degli spazi.</p>
 
 <ul>
-    <li>Ciao</li>
-    <li>Ciao</li>
-    <li>Ciao</li>
-    <li>Ciao</li>
-    <li>Ciao</li>
+    <li>Scegli la modalità utilizzando i due bottoni presenti sotto</li>
+
+    <li>Se hai scelto multiplayer, il giocatore due inserisce una parola facendo attenzione che il 
+    giocatore uno non la riesca a vedere</li>
+    
+    <li>Verrai reindirizzato ad una pagina in cui puoi inserire una singola lettera con il box a destra
+    o l'intera frase con il box a sinistra. Hai a disposizione sette errori, dopo i quali avrai perso
+    la partita.</li>
+
+    <li>Alla fine del gioco, in caso di vittoria potrai inserire il tuo username ed il tuo nome verrà
+    visualizzato nella <a href="leaderboard.php">leaderboard</a>.</li>
 </ul>
