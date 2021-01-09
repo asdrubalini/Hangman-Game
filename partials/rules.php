c74156f59f4495c1702135931ae088a2763fd782
Giovanni <indirizzoemail@mail.com>
2021-01-09 22:54:31 +0100

    update rules

--- partials/rules.php
+++ partials/rules.php
@@ -6,14 +6,15 @@ della frase e la posizione degli spazi. I tentativi a disposizione sono solo set
 dall'immagine sulla sinistra.</p>
 
 <ul>
-    <li>Scegli la modalità utilizzando i due bottoni presenti sotto</li>
+    <li>Scegli la modalità utilizzando i due bottoni presenti sotto. Alternativamente, se hai un'altra partita
+    in corso la puoi riprendere con il bottone in alto a destra;</li>
 
     <li>Se hai scelto multiplayer, il giocatore due inserisce una parola facendo attenzione che il 
-    giocatore uno non la riesca a vedere</li>
+    giocatore uno non la riesca a vedere;</li>
     
     <li>Verrai reindirizzato ad una pagina in cui puoi inserire una singola lettera con il box a destra
     o l'intera frase con il box a sinistra. Hai a disposizione sette errori, dopo i quali avrai perso
-    la partita.</li>
+    la partita;</li>
 
     <li>Alla fine del gioco, in caso di vittoria potrai inserire il tuo username ed il tuo nome verrà
     visualizzato nella <a href="leaderboard.php">leaderboard</a>.</li>
